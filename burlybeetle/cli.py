e23419b53405aaf1d350ba7b4e8377e86aa50a75
Jorge Gallegos <kad@blegh.net>
2020-03-17 17:07:06 -0700

    s/servo//

--- burlybeetle/cli.py
+++ burlybeetle/cli.py
@@ -5,6 +5,4 @@ from fabric.main import main as _main
 
 
 def main():
-    # dirname(__file__) should always be the 'servo' package dir.
-    # Tell Fabric's main() to explicitly use that fabfile path.
     _main([os.path.join(os.path.dirname(__file__), 'fabfile.py')])
